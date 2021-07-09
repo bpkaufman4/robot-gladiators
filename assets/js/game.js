e5f4a126b784e0b4228e92677c5aff8caaa2e6e5
Brian Kaufman <briankaufman@Brians-MBP-5.attlocal.net>
2021-07-09 15:32:01 -0500

    fixed skip bug issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -49,6 +49,31 @@ var enemyInfo = [
   }
 ]
 
+var fightOrSkip = function() {
+  // ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.')
+
+  // Conditional recursive funtion call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.")
+    return fightOrSkip();
+  }
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm the player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //if yes leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight.");
+      // subtract money from playerMoney for skipping
+      playerInfo.money = playerInfo.money - 10;
+      shop();
+    }
+  }
+}
+
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyInfo) {
